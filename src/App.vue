4927815623012bae828120ece7fe184c3689a2a1
Rodrigo Esteban Valenzuela Cardenas <30939648+ZatmanUfro@users.noreply.github.com>
2021-10-14 17:14:53 -0300

    Tarea vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12"><h1>Productos</h1></div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <product-form @add-product="addProduct" />
+        <product-table
+          :products="products"
+          @delete-product="deleteProduct"
+          @update-product="updateProduct"
+        />
+      </div>
+    </div>
+  </div>
+</template> 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ProductTable from "@/components/ProductTable.vue";
+import ProductForm from "@/components/ProductForm.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: { ProductTable, ProductForm },
+  data() {
+    return {
+      products: [
+        { name: "Uranio", description: "Para uso casero", price: 500 },
+        { name: "Unicornio", description: "3 cuernos", price: 600 },
+        {
+          name: "Dinosaurio",
+          description: "Velociraptor 5 metros",
+          price: 340,
+        },
+      ],
+    };
+  },
+  methods: {
+    addProduct(product) {
+      this.products = [...this.products, product];
+    },
+    deleteProduct(index) {
+      this.products.splice(index, 1);
+    },
+    updateProduct(editIndex, editProduct) {
+      this.products = this.products.map((product, index) =>
+        index === editIndex ? editProduct : product
+      );
+    },
+  },
+};
 </script>
 
 <style>
